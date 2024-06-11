7e88afe51e1495e178e7c61f05a4921e0dc9f42d
Andrey Pfau <andreypfau@ton.org>
2024-06-11 17:44:30 +0400

    fix router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import NavigationBar from './Components/NavigationBar';
 import MainContent from './Page/MainContent';
 import RulesPage from './Page/RulesPage';
@@ -10,15 +10,13 @@ import Footer from "./Components/Footer";
 const App = () => {
     return (
         <>
-            <Router>
-                <NavigationBar/>
-                <Routes>
-                    <Route exact path="/" element={<MainContent/>}/>
-                    <Route path="/Page/RulesPage" element={<RulesPage/>}/>
-                    <Route path="/Page/GuildPage" element={<GuildPage/>}/>
-                    <Route path="/Page/LogIn" element={<LogIn/>}/>
-                </Routes>
-            </Router>
+            <NavigationBar/>
+            <Routes>
+                <Route exact path="/" element={<MainContent/>}/>
+                <Route path="/Page/RulesPage" element={<RulesPage/>}/>
+                <Route path="/Page/GuildPage" element={<GuildPage/>}/>
+                <Route path="/Page/LogIn" element={<LogIn/>}/>
+            </Routes>
 
             <Footer/>
         </>

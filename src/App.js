d30a43b302d0eafe42d66fd0e3ff74bb6680d769
Andrey Pfau <andreypfau@ton.org>
2024-06-12 12:41:57 +0400

    map

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import LogIn from './Page/LogIn';
 import Footer from "./Components/Footer";
 import Profile from './Page/Profile';
 import NotFoundPage from "./Page/404";
+import MapPage from "./Page/MapPage";
 
 const App = () => {
     return (
@@ -19,9 +20,9 @@ const App = () => {
                 <Route path="/guild" element={<GuildPage/>}/>
                 <Route path="/login" element={<LogIn/>}/>
                 <Route path="/profile" element={<Profile/>}/>
+                <Route path="/map" element={<MapPage/>}/>
 
                 <Route path="*" element={<NotFoundPage/>}/>
-
             </Routes>
 
             <Footer/>
---
+++ src/Page/MapPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function MapPage() {
+    return (
+        <iframe src="https://map.pfaumc.io/" width="100%" height="100%">
+        </iframe>
+    )
+}

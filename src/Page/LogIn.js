89c33a1d9163206169dc6b9ee0636ce25393a32e
deadlysunset <fafarh09@gmail.com>
2024-07-02 07:11:21 +0500

    mobile version

--- src/Page/LogIn.js
+++ src/Page/LogIn.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./Profile.scss"
+import "./Login.scss"
 import Auth from "../Components/Auth";
 import SocialIcons from "../Components/SocialIcons"
 import Fireflies from '../Components/Fireflies';
@@ -7,7 +7,7 @@ import Fireflies from '../Components/Fireflies';
 export default function Profile() {
     return (
         <div>
-            <div className="profile">
+            <div className="login">
                 <Auth/>
                 <SocialIcons/>
 
